8b89a4dd0c63f27495a0e1e16194374e7d14d851
stharavi01 <sanayashe@gmail.com>
2024-02-25 12:08:10 +0545

    create gameloop

--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -6,53 +6,55 @@ import java.awt.event.MouseMotionListener;
 
 import main.GamePanel;
 
-public class MouseInputs implements MouseListener, MouseMotionListener{
-	
+public class MouseInputs implements MouseListener, MouseMotionListener {
+
 	private GamePanel gamePanel;
+
 	public MouseInputs(GamePanel gamePanel) {
-	this.gamePanel = gamePanel;
-	}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		System.out.println("Mouse clicked");
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		this.gamePanel = gamePanel;
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		gamePanel.setRectPosition(e.getX(), e.getY());
-		
+//		gamePanel.setRectPos(e.getX(), e.getY());
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+//		System.out.println("Mouse clicked!");
+		gamePanel.spawnRect(e.getX(),e.getY());
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
